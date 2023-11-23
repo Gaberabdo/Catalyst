fdb96e2c59b2a891bfd1b30507e1701c587345d0
mok7tar-22 <93128332+mok7tar-22@users.noreply.github.com>
2023-11-23 20:29:25 +0200

    Update week_model.dart

--- lib/features/chart/data/week_model.dart
+++ lib/features/chart/data/week_model.dart
@@ -1,35 +1,125 @@
-class WeekModel {
+// class MetalData {
+//   bool success;
+//   int code;
+//   Metal metal;
+//
+//   MetalData({
+//     required this.success,
+//     required this.code,
+//     required this.metal,
+//   });
+//
+//   factory MetalData.fromJson(Map<String, dynamic> json) {
+//     return MetalData(
+//       success: json['success'],
+//       code: json['code'],
+//       metal: Metal.fromJson(json['metal']),
+//     );
+//   }
+//
+//   Map<String, dynamic> toJson() {
+//     return {
+//       'success': success,
+//       'code': code,
+//       'metal': metal.toJson(),
+//     };
+//   }
+// }
+//
+// class Metal {
+//   String id;
+//   int pd;
+//   int pt;
+//   int rh;
+//   DateTime date;
+//   List<int> pdDaily;
+//   List<int> ptDaily;
+//   List<int> rhDaily;
+//
+//   Metal({
+//     required this.id,
+//     required this.pd,
+//     required this.pt,
+//     required this.rh,
+//     required this.date,
+//     required this.pdDaily,
+//     required this.ptDaily,
+//     required this.rhDaily,
+//   });
+//
+//   factory Metal.fromJson(Map<String, dynamic> json) {
+//     return Metal(
+//       id: json['_id'],
+//       pd: json['pd'],
+//       pt: json['pt'],
+//       rh: json['rh'],
+//       date: DateTime.parse(json['date']),
+//       pdDaily: List<int>.from(json['pdDaily']),
+//       ptDaily: List<int>.from(json['ptDaily']),
+//       rhDaily: List<int>.from(json['rhDaily']),
+//     );
+//   }
+//
+//   Map<String, dynamic> toJson() {
+//     return {
+//       '_id': id,
+//       'pd': pd,
+//       'pt': pt,
+//       'rh': rh,
+//       'date': date.toIso8601String(),
+//       'pdDaily': pdDaily,
+//       'ptDaily': ptDaily,
+//       'rhDaily': rhDaily,
+//     };
+//   }
+// }
+
+//--------
+class MetalData {
   bool success;
   int code;
-  List<double> pdHistory;
-  List<double> ptHistory;
-  List<double> rhHistory;
+  Metal metal;
 
-  WeekModel({
+  MetalData({
     required this.success,
     required this.code,
-    required this.pdHistory,
-    required this.ptHistory,
-    required this.rhHistory,
+    required this.metal,
   });
 
-  factory WeekModel.fromJson(Map<String, dynamic> json) {
-    return WeekModel(
+  factory MetalData.fromJson(Map<String, dynamic> json) {
+    return MetalData(
       success: json['success'],
       code: json['code'],
-      pdHistory: List<double>.from(json['pdHistory']),
-      ptHistory: List<double>.from(json['ptHistory']),
-      rhHistory: List<double>.from(json['rhHistory']),
+      metal: Metal.fromJson(json['metal']),
     );
   }
+}
 
-  Map<String, dynamic> toJson() {
-    return {
-      'success': success,
-      'code': code,
-      'pdHistory': pdHistory,
-      'ptHistory': ptHistory,
-      'rhHistory': rhHistory,
-    };
+class Metal {
+  List<double> pdDaily;
+  List<double> ptDaily;
+  List<double> rhDaily;
+  List<String> date;
+
+  Metal({
+    required this.pdDaily,
+    required this.ptDaily,
+    required this.rhDaily,
+    required this.date,
+  });
+
+  factory Metal.fromJson(Map<String, dynamic> json) {
+    return Metal(
+      pdDaily:
+          (json['pdDaily'] as List).map((e) => (e as num).toDouble()).toList(),
+      ptDaily:
+          (json['ptDaily'] as List).map((e) => (e as num).toDouble()).toList(),
+      rhDaily:
+          (json['rhDaily'] as List).map((e) => (e as num).toDouble()).toList(),
+      date: List<String>.from(json['date'].map((dateString) {
+        DateTime dateTime = DateTime.parse(dateString);
+        return '${dateTime.hour}:${dateTime.minute}';
+      })),
+    );
   }
 }
