7d10982bddf8c02a6e28ae1eb052b3ebe3546420
mok7tar-22 <93128332+mok7tar-22@users.noreply.github.com>
2023-11-23 20:30:22 +0200

    Update week_model.dart

--- lib/features/chart/data/week_model.dart
+++ lib/features/chart/data/week_model.dart
@@ -1,48 +1,31 @@
-class MetalData {
-  bool success;
-  int code;
-  Metal metal;
+class WeekModel {
+  final bool success;
+  final int code;
+  final List<double> pdHistory;
+  final List<double> ptHistory;
+  final List<double> rhHistory;
 
-  MetalData({
+  WeekModel({
     required this.success,
     required this.code,
-    required this.metal,
+    required this.pdHistory,
+    required this.ptHistory,
+    required this.rhHistory,
   });
 
-  factory MetalData.fromJson(Map<String, dynamic> json) {
-    return MetalData(
+  factory WeekModel.fromJson(Map<String, dynamic> json) {
+    return WeekModel(
       success: json['success'],
       code: json['code'],
-      metal: Metal.fromJson(json['metal']),
-    );
-  }
-}
-
-class Metal {
-  List<double> pdDaily;
-  List<double> ptDaily;
-  List<double> rhDaily;
-  List<String> date;
-
-  Metal({
-    required this.pdDaily,
-    required this.ptDaily,
-    required this.rhDaily,
-    required this.date,
-  });
-
-  factory Metal.fromJson(Map<String, dynamic> json) {
-    return Metal(
-      pdDaily:
-          (json['pdDaily'] as List).map((e) => (e as num).toDouble()).toList(),
-      ptDaily:
-          (json['ptDaily'] as List).map((e) => (e as num).toDouble()).toList(),
-      rhDaily:
-          (json['rhDaily'] as List).map((e) => (e as num).toDouble()).toList(),
-      date: List<String>.from(json['date'].map((dateString) {
-        DateTime dateTime = DateTime.parse(dateString);
-        return '${dateTime.hour}:${dateTime.minute}';
-      })),
+      pdHistory: (json['pdHistory'] as List)
+          .map((e) => (e as num).toDouble())
+          .toList(),
+      ptHistory: (json['ptHistory'] as List)
+          .map((e) => (e as num).toDouble())
+          .toList(),
+      rhHistory: (json['rhHistory'] as List)
+          .map((e) => (e as num).toDouble())
+          .toList(),
     );
   }
 }
